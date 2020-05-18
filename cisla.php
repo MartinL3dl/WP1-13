b2d51fdad597f13eb24380afcbc7d51ed21fa9eb
MartinL3dl <martin.ledl@student.ossp.cz>
2020-05-18 17:25:25 +0200

    kdo mohl očekávát že filter_input(input_post) se má ten (INPUT_POST) napsat s capslockem ?
    See the pyramids along the Nile
    Watch the sunrise from a tropic isle
    Just remember darling all the while
    You belong to me
    See the market place in old Algiers
    Send me photographs and souvenirs
    Just remember when a dream appears
    You belong to me
    And Ill be so alone without you
    Maybe you'll be lonesome too and blue
    Fly the ocean in a silver plane
    See the jungle when it's wet with rain
    Just remember till you're home again
    You belong to me
    Ill be so alone without you
    Maybe you'll be lonesome too and blue
    Fly the ocean in a silver plane
    See the jungle when it's wet with rain
    Just remember till you're home again
    You belong to me
    
    - M 2 M

---
+++ cisla.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="submit_cisla.php" method="post">
+    <input type="number" name="A" id="i-A">
+    <input type="number" name="B" id="i-B">
+<br>
+
+<input type="submit" value="odeslat">    
+    </form>
+
+
+</body>
+</html>
---
+++ submit_cisla.php
@@ -0,0 +1,38 @@
+<?php
+$A = filter_input(INPUT_POST, 'A');
+$B = filter_input(INPUT_POST, 'B');
+$plus = $A + $B ;
+$minus = $A - $B ;
+$multiply = $A * $B ;
+$divided = $A / $B ;
+
+$arterplus = $B + $A ;
+$arterminus = $B - $A ;
+$artermultiply = $B * $A ;
+$arterdivided = $B / $A ;
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Formulář byl odeslán</p>
+    <ul>
+    <li>sčítání : <?= $A ?> + <?= $B ?> = <?=$plus ?></li>
+    <li>odčítání : <?= $A ?> - <?= $B ?> = <?=$minus ?></li>
+    <li>násobení : <?= $A ?> X <?= $B ?> = <?=$multiply ?></li>
+    <li>Dělení : <?= $A ?> : <?= $B ?> = <?=$divided ?></li>
+
+    <p>Obrácené výpočty</p>
+
+    <li>Sčítání : <?= $B ?> + <?= $A ?> = <?=$arterplus ?></li>
+    <li>odčítání : <?= $B ?> - <?= $A ?> = <?=$arterminus ?></li>
+    <li>násobení : <?= $B ?> X <?= $A ?> = <?=$artermultiply ?></li>
+    <li>Děleno : <?= $B ?> : <?= $A ?> = <?=$arterdivided ?></li>
+        
+    </ul>
+</body>
+</html>
